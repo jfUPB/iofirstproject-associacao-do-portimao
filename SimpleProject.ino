d38b096c5891502cf952f48a897746e0ec97ecda
Kayre <kayre@pop-os.localdomain>
2022-08-16 08:50:59 -0500

    Update file code

---
+++ SimpleProject.ino
@@ -0,0 +1,96 @@
+void task1()
+{
+    // Definición de estados y variable de estado
+    enum class Task1States
+    {
+        INIT,
+        WAIT_TIMEOUT
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    // Definición de variables static (conservan
+    // su valor entre llamadas a task1)
+    static uint32_t lasTime = 0;
+
+    // Constantes
+
+    constexpr uint32_t INTERVAL = 1000;
+    constexpr uint8_t button1Pin = 12;
+    constexpr uint8_t button2Pin = 13;
+    constexpr uint8_t ledRed = 25;
+    constexpr uint8_t ledGreen = 14;
+    constexpr uint8_t ledBlue = 27;
+    constexpr uint8_t ledYellow = 26;
+
+    // MÁQUINA de ESTADOS
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        pinMode(button1Pin, INPUT_PULLUP);
+        pinMode(button2Pin, INPUT_PULLUP);
+        pinMode(ledRed, OUTPUT);
+        pinMode(ledGreen, OUTPUT);
+        pinMode(ledBlue, OUTPUT);
+        pinMode(ledYellow, OUTPUT);
+        lasTime = millis();
+        task1State = Task1States::WAIT_TIMEOUT;
+
+        break;
+    }
+    case Task1States::WAIT_TIMEOUT:
+    {
+        uint8_t btn1State = digitalRead(button1Pin);
+        uint8_t btn2State = digitalRead(button2Pin);
+        uint32_t currentTime = millis();
+
+        // Evento 1:
+        if ((currentTime - lasTime) >= INTERVAL)
+        {
+            lasTime = currentTime;
+            printf("btn1: %d,btn2: %d\n", btn1State, btn2State);
+        }
+
+        // Evento 2
+        if (btn1State == LOW & btn2State == LOW)
+            digitalWrite(ledRed, HIGH);
+            else
+            digitalWrite(ledRed, LOW);
+             
+            
+        // Evento 3
+        if (btn1State == LOW & btn2State == HIGH)
+            digitalWrite(ledGreen, HIGH);
+            else
+            digitalWrite(ledGreen, LOW);
+        // Evento 4
+        if (btn1State == HIGH & btn2State == LOW)
+            digitalWrite(ledBlue, HIGH);
+            else
+            digitalWrite(ledBlue, LOW);
+        // Evento 5
+        if (btn1State == HIGH & btn2State == HIGH)
+            digitalWrite(ledYellow, HIGH);
+            else
+            digitalWrite(ledYellow, LOW);
+
+        break;
+    }
+    default:
+    {
+        Serial.println("Error");
+    }
+    }
+}
+
+void setup()
+{
+    task1();
+}
+
+void loop()
+{
+    task1();
+}
